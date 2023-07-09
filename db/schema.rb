87eb12210daed6fd9d6ac00b8061622f9c3d09cd
samsondanicah <danicahsamson26@gmail.com>
2023-07-09 14:32:43 +0800

    Title Content Address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_065350) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_101616) do
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
