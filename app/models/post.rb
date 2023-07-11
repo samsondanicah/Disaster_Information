d833d43a6518e6668560aa49eb7aa6491d7c4777
samsondanicah <danicahsamson26@gmail.com>
2023-07-11 14:14:46 +0800

    add user id is to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_100239) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_022105) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_100239) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
