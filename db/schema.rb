9bf62944312ae12c084ad338040153b49a709757
samsondanicah <danicahsamson26@gmail.com>
2023-07-11 16:21:44 +0800

    editing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_055317) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_080529) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_055317) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.datetime "deleted_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
